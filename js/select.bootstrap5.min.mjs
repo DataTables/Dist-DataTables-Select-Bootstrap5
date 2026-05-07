a5f09eefa4ae37681d993a3d230a2b9d8b644557
dtbuild <dtbuild@rigil>
2026-05-07 14:01:55 +0000

    a46f61b466102bb85f9c0ff45772e0e300d59d68
    Sync to source repo @76c586acb7863acb18de62411d11f481e0d44a0f

--- js/select.bootstrap5.min.mjs
+++ js/select.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
-/*! Bootstrap 5 styling wrapper for Select
- * © SpryMedia Ltd - datatables.net/license
+/*! Select Bootstrap 5 styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
-import jQuery from"jquery";import DataTable from"datatables.net-bs5";import select from"datatables.net-select";let $=jQuery;DataTable.select.classes.checkbox="form-check-input dt-select-checkbox";export default DataTable;
+import DataTable from"datatables.net-bs5";import select from"datatables.net-select";DataTable.select.classes.checkbox="form-check-input dt-select-checkbox";export default DataTable;
