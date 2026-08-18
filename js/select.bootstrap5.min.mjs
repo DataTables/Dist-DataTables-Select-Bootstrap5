a7d64892c01f071a57e7158f8c848bf949ff25f8
dtbuild <dtbuild@rigil>
2026-08-18 10:44:54 +0000

    947e7c05b07ed51310748c0017288147906a54c5 Release 4.0.1
    Sync to source repo @947e7c05b07ed51310748c0017288147906a54c5

--- js/select.bootstrap5.min.mjs
+++ js/select.bootstrap5.min.mjs
@@ -1,4 +1,4 @@
-/*! Select Bootstrap 5 styling 4.0.0 for DataTables
+/*! Select Bootstrap 5 styling 4.0.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs5";import select from"datatables.net-select";DataTable.select.classes.checkbox="form-check-input dt-select-checkbox";export default DataTable;
